e146fc5bce308e3b0d20a878fbe642d828c3bb3d
CharlotteHuliyi <lh2847@nyu.edu>
2022-10-13 22:59:27 -0400

    fourth with added recipe

--- src/main/ReadRecipes.java
+++ src/main/ReadRecipes.java
@@ -7,15 +7,15 @@ import java.util.ArrayList;
 public class ReadRecipes {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         FileInputStream fis = new FileInputStream("recipeBook.ser");
-        ObjectInputStream ois = new ObjectInputStream(fis);
+        try (ObjectInputStream ois = new ObjectInputStream(fis)) {
+            ArrayList<Recipe> recipeBook = new ArrayList<>();
+            recipeBook = (ArrayList<Recipe>) ois.readObject();
 
-        ArrayList<Recipe> recipeBook = new ArrayList<>();
-        recipeBook = (ArrayList<Recipe>) ois.readObject();
-
-        for (int i = 0; i < recipeBook.size(); i++) {
-            //System.out.println(recipeBook.get(i)); //this will call toString()
-            recipeBook.get(i).printEverything();
-            System.out.println("##############################################################");
+            for (int i = 0; i < recipeBook.size(); i++) {
+                //System.out.println(recipeBook.get(i)); //this will call toString()
+                recipeBook.get(i).printEverything();
+                System.out.println("##############################################################");
+            }
         }
     }
 }
